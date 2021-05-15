8f4e2ab7abbf1d3a338c47c70e089662a9475ad9
Margarita Krutikova <margaretkru@gmail.com>
2021-05-15 16:09:15 +0200

    Add farmer deployment script

---
+++ notification-service/FarmerSetup/Program.fs
@@ -0,0 +1,18 @@
+﻿open Farmer
+open Farmer.Builders
+
+let notificationServiceWebApp = webApp {
+    name "food-order-notification-service"
+    sku WebApp.Sku.F1
+    zip_deploy @""
+}
+
+let deployment = arm {
+    location Location.NorthEurope
+    add_resource notificationServiceWebApp
+}
+
+deployment |> Writer.quickWrite "notification-service-arm-template"
+
+deployment |> Deploy.execute "food-order-notification-service-rg" [] |> ignore
+
