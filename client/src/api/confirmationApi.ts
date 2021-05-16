076cae729299a24a5840223535d6c8b45fab2021
Margarita Krutikova <margaretkru@gmail.com>
2021-05-16 18:12:42 +0200

    Fix env variables for client apis

--- client/src/api/confirmationApi.ts
+++ client/src/api/confirmationApi.ts
@@ -1,7 +1,9 @@
-const baseUrl = process.env.REACT_APP_API_CONFIRMATION_URL as string
+const baseUrl = process.env.REACT_APP_CONFIRMATION_API_URL as string
 
 const getConfirmationApiUrl = (orderId: number) =>
-  `/notification/order/${orderId}/send-confirmation`
+  `${
+    process.env.REACT_APP_CONFIRMATION_API_URL as string
+  }/notification/order/${orderId}/send-confirmations`
 
 export type ConfirmationResult =
   | { kind: "success" }
@@ -24,12 +26,12 @@ export const sendConfirmation = async (
     const data = isJson && (await response.json())
     if (!response.ok) {
       // get error message from body or default to response status
-      const error = (data && data.message) || response.status
+      const error = (data && data.message) || response.statusText
       return { kind: "error", error }
     }
     return { kind: "success" }
   } catch (e) {
-    console.log(e)
-    return { kind: "error", error: e.toString() }
+    console.error(e)
+    return { kind: "error", error: "Unknown error has occurred" }
   }
 }
