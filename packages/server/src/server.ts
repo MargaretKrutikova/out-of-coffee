cf862d276b74c9d801b3c8fb3b73660ac440b7d9
Michael Zetterholm <michael.zetterholm@antura.se>
2021-09-24 16:36:23 +0200

    fix static content route

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -4,8 +4,9 @@ import path from 'path';
 const app = express();
 const port = 8008;
 const serverDir = process.cwd();
-const buildDir = path.join(serverDir, "../client/public");
-app.use(express.static(path.join(__dirname, 'build')));
+const buildDir = path.join(serverDir, "../client/build");
+
+app.use(express.static(buildDir));
 
 // @ts-ignore
 app.get('/ping', function (req, res) {
