117743a50625a95f0725aa05c16df84241743f81
Victor Ek <viceki@antura.se>
2021-10-08 16:54:19 +0200

    BorderCard WIP (dont judge too hard)

--- packages/client/src/views/Orders.tsx
+++ packages/client/src/views/Orders.tsx
@@ -1,7 +1,26 @@
+import { BorderCard } from "../components"
+
 export const Orders = () => {
     return (
         <div>
-            Beställningar!
+            <BorderCard title="Nästa order" subTitle="levereras snart kanske">
+                Beställningar
+                <br />
+                Beställningar
+                <br />
+                Beställningar
+                <br />
+                Beställningar
+                <br />
+                Beställningar
+                <br />
+                Beställningar
+                <br />
+                Beställningar
+                <br />
+                Beställningar
+                <br />
+            </BorderCard>
         </div>
     )
 }
