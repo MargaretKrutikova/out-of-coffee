f663917b637d1f34a2fd1b06323c1c2f01e530af
Alexander Lindberg <alexanderlndbrg@gmail.com>
2021-10-08 17:03:55 +0200

    WIP

--- docker/db/init-db.sh
+++ docker/db/init-db.sh
@@ -2,9 +2,9 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER oos;
-    CREATE DATABASE oos;
-    GRANT ALL PRIVILEGES ON DATABASE oos TO oos;
+    CREATE USER docker;
+    CREATE DATABASE docker;
+    GRANT ALL PRIVILEGES ON DATABASE docker TO postgres;
 		CREATE TABLE IF NOT EXISTS items (
 					id INTEGER UNIQUE PRIMARY KEY,
 					name TEXT UNIQUE NOT NULL,
