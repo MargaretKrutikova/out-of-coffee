b6842ecfcd9c9961b8ae8f20698a72c0b7687b2c
Alexander Lindberg <alexanderlndbrg@gmail.com>
2021-10-08 16:57:22 +0200

    use oos user/database

--- docker/db/init-db.sh
+++ docker/db/init-db.sh
@@ -2,9 +2,9 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER docker;
-    CREATE DATABASE docker;
-    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+    CREATE USER oos;
+    CREATE DATABASE oos;
+    GRANT ALL PRIVILEGES ON DATABASE oos TO oos;
 		CREATE TABLE IF NOT EXISTS items (
 					id INTEGER UNIQUE PRIMARY KEY,
 					name TEXT UNIQUE NOT NULL,
