767ef2ef68563f0e0eb9387873a63c1fe08093c1
Michael Zetterholm <michael.zetterholm@antura.se>
2021-10-08 16:21:27 +0200

    Add tabs style

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import '@reach/tabs/styles.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
