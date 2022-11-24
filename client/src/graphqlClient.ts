61d0c86460279a9951290df996b4ef159d2c5dc2
Margarita Krutikova <margaretkru@gmail.com>
2022-11-24 15:09:07 +0100

    Migrate to new api

--- client/src/graphqlClient.ts
+++ client/src/graphqlClient.ts
@@ -1,6 +1,11 @@
-import { createClient } from "urql"
+import { createClient } from "urql";
 
-console.log("url", process.env.REACT_APP_ORDER_API_URL)
+console.log("url", process.env.REACT_APP_ORDER_API_URL);
 export const client = createClient({
   url: process.env.REACT_APP_ORDER_API_URL as string,
-})
+  fetchOptions: {
+    headers: {
+      "x-hasura-admin-secret": process.env.REACT_APP_API_KEY as string,
+    },
+  },
+});
